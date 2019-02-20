3d0dc9dc2d02a24908695c9a89e069b499cd5cf1
sese22 <tienloinguyen22@gmail.com>
2019-02-20 23:53:41 +0700

    update advanced manu

--- js-practices/handout/Practice3/generatePractice.js
+++ js-practices/handout/Practice3/generatePractice.js
@@ -1,13 +1,39 @@
 'use strict'
 
 function generate(testLengthArray){
-  return Array.from({length : testLengthArray.length})
-    .map(item => ({
-      input: Array.from({length: item}).map(item => []),
-      target: 0,
-      output: -1
-    })
-  ); // Remove this line and change to your own algorithm
+  const arr = [];
+
+  for(let i = 0; i < testLengthArray.length; i++){
+    if(i == 1){
+      let n = testLengthArray[i];
+      let arr1 = [];
+      for(let j = 0; j < n; j++){
+        arr1[j] = j;
+      }
+
+      var obj = {
+        input: arr1,
+        target: n + 8,
+        output: -1
+      }
+      arr[i] = obj;
+    }
+    else{
+      let n = testLengthArray[i];
+      let arr1 = [];
+      for(let j = 0; j < n; j++){
+        arr1[j] = j;
+      }
+
+      let obj = {
+        input: arr1,
+        target: n - 1,
+        output: n - 1
+      }
+      arr[i] = obj;
+    }
+  }
+  return arr;
 }
 
 module.exports = generate
