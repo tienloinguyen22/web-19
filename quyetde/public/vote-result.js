e0be16197afd879cfd039e2b9b8afae309a2e619
sese22 <tienloinguyen22@gmail.com>
2019-03-03 23:38:08 +0700

    update quyet de

---
+++ quyetde/public/vote-result.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+  const pathname = window.location.pathname;
+  const questionId = pathname.split('/')[pathname.split('/').length - 1];
+
+  $.ajax({
+    url: `/get-question-by-id?questionId=${questionId}`,
+    type: 'GET',
+    success: (data) => {
+      if (data.id) {
+        document.getElementById('question-content').innerText = data.content;
+        document.getElementById('total-votes').innerText = data.yes + data.no;
+
+        const yesPercent = data.yes / (data.yes + data.no) * 100;
+        const noPercent = 100 - yesPercent;
+        document.getElementById('yes-percent').innerText = `${yesPercent.toFixed(3)}%`;
+        document.getElementById('no-percent').innerText = `${noPercent}%`;
+      } else {
+        document.getElementById('question-content').innerText = 'Question not found';
+      }
+    },
+    error: (error) => {
+      console.log(error);
+    },
+  });
+})
--- quyetde/server.js
+++ quyetde/server.js
@@ -9,7 +9,47 @@ server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
 server.get('/', (req, res) => {
-  res.status(200).send('Hello expressjs 123123');
+  fs.readFile('./data.json', (error, data) => {
+    if (error) {
+      res.status(500).send('Internal server error');
+    }
+
+    const questions = JSON.parse(data);
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const randomQuestion = questions[randomIndex];
+    console.log(randomIndex, randomQuestion);
+
+    res.status(200).send(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Quyet De</title>
+      </head>
+      <body>
+        <h2>${randomQuestion.content}</h2>
+
+        <div>
+          <form name='yes' method='get' action='/vote/${randomQuestion.id}/yes'>
+            <input type='submit' value='yes' />
+          </form>
+          <form name='no' method='get' action='/vote/${randomQuestion.id}/no'>
+            <input type='submit' value='no' />
+          </form>
+        </div>
+
+        <div>
+          <button id='question-result'>Result</button>
+          <button id='other-question'>Other question</button>
+        </div>
+
+        <script src='./public/index.js'></script>
+      </body>
+      </html>
+    `);
+  });
 });
 
 server.get('/create-question', (req, res) => {
@@ -39,14 +79,63 @@ server.post('/create-question', (req, res) => {
       res.status(201).end('Sucess');
     });
   });
-  // id, content, yes, no, createdAt
-  // const newQuestion = {
-  //   id: '',
-  //   content: '',
-  //   yes: 0,
-  //   no: 0,
-  //   createdAt: '',
-  // };
+});
+
+server.get('/vote/:questionId/:vote', async (req, res) => {
+  const { questionId, vote } = req.params;
+  console.log(questionId, vote);
+
+  fs.readFile('./data.json', (error, data) => {
+    if (error) {
+      res.status(500).send('Internal server error');
+    }
+
+    const questions = JSON.parse(data);
+    for (let item of questions) {
+      if (item.id === Number(questionId)) {
+        vote === 'yes' ? item.yes += 1 : item.no += 1;
+        break;
+      }
+    }
+
+    fs.writeFile('./data.json', JSON.stringify(questions), (err) => {
+      if (err) {
+        res.status(500).send('Internal server error');
+      }
+
+      res.status(200).send('Update question success');
+    });
+  });
+});
+
+server.get('/result/:questionId', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, './public/vote-result.html'));
+});
+
+server.get('/get-question-by-id', (req, res) => {
+  console.log(req.query);
+  const questionId = req.query.questionId;
+
+  fs.readFile('./data.json', (error, data) => {
+    if (error) {
+      res.status(500).send('Internal server error');
+    }
+
+    const questions = JSON.parse(data);
+    let selectQuestion;
+    for (let item of questions) {
+      if (item.id === Number(questionId)) {
+        selectQuestion = item;
+        break;
+      }
+    }
+
+    if (selectQuestion) {
+      res.status(200).json(selectQuestion);
+    } else {
+      res.status(200).json({message: 'Question not found'});
+    }
+  });
 });
 
 server.listen(3000, (error) => {
